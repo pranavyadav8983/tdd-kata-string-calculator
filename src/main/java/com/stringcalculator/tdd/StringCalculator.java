cb7d74cbf2a8b8fdae8b7301735e41bf3ec12531
Pranav <pranavyadav8983@gmail.com>
2021-02-21 00:49:35 +0530

    Previously failed test case is made success along with code refactoring.

--- src/main/java/com/stringcalculator/tdd/StringCalculator.java
+++ src/main/java/com/stringcalculator/tdd/StringCalculator.java
@@ -2,17 +2,16 @@ package com.stringcalculator.tdd;
 
 public class StringCalculator {
 	public int Add(String numbers) {
-		String stringArray[]=numbers.split(",");
-		if(numbers.isEmpty()) {
-			return 0;
+		int sum = 0;
+		String stringArray[] = numbers.split(",");
+		if (numbers.isEmpty()) {
+			sum = 0;
+		} else {
+			for (String number : stringArray) {
+				sum = sum + Integer.parseInt(number);
+			}
 		}
-		if(stringArray.length==1) {
-			return Integer.parseInt(stringArray[0]);
-		}
-		if(stringArray.length==2) {
-			return Integer.parseInt(stringArray[0])+Integer.parseInt(stringArray[1]);
-		}
-		return 1;
+		return sum;
 	}
-	
+
 }
